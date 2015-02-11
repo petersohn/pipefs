561cc20e661b464f1f028aeac90f09ea736c8f4f
Péter Szabados <peter.szabados@ericsson.com>
2015-02-11 13:55:59 +0100

    Make unit test leak-free

--- UnitTest/utilTest.cpp
+++ UnitTest/utilTest.cpp
@@ -6,31 +6,39 @@
 
 BOOST_AUTO_TEST_SUITE(UtilTest)
 
-BOOST_AUTO_TEST_SUITE(translate_suffix_test)
+struct ResultStringFixture {
+	char* result = nullptr;
+
+	~ResultStringFixture()
+	{
+		free(result);
+	}
+};
+
+BOOST_FIXTURE_TEST_SUITE(translate_suffix_test, ResultStringFixture)
 
 BOOST_AUTO_TEST_CASE(find_suffix)
 {
-	char* result = translate_suffix("this is first", "first", "second");
+	result = translate_suffix("this is first", "first", "second");
 	BOOST_REQUIRE(result);
 	BOOST_CHECK_EQUAL(std::string{result}, "this is second");
-	free(result);
 }
 
 BOOST_AUTO_TEST_CASE(no_suffix)
 {
-	char* result = translate_suffix("this is first", "fir", "second");
+	result = translate_suffix("this is first", "fir", "second");
 	BOOST_CHECK(!result);
 }
 
 BOOST_AUTO_TEST_CASE(too_long_suffix)
 {
-	char* result = translate_suffix("this is first", "this is firstt", "second");
+	result = translate_suffix("this is first", "this is firstt", "second");
 	BOOST_CHECK(!result);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
 
-struct TranslateFileFixture {
+struct TranslateFileFixture: ResultStringFixture {
 	const char* filename = "existing_file.xxx";
 	TranslateFileFixture()
 	{
@@ -47,27 +55,26 @@ BOOST_FIXTURE_TEST_SUITE(translate_file_test, TranslateFileFixture)
 
 BOOST_AUTO_TEST_CASE(succcessful_translation)
 {
-	char* result = translate_file("existing_file.txt", "xxx", "txt");
+	result = translate_file("existing_file.txt", "xxx", "txt");
 	BOOST_REQUIRE(result);
 	BOOST_CHECK_EQUAL(std::string{result}, filename);
-	free(result);
 }
 
 BOOST_AUTO_TEST_CASE(file_not_exists)
 {
-	char* result = translate_file("not_existing_file.txt", "xxx", "txt");
+	result = translate_file("not_existing_file.txt", "xxx", "txt");
 	BOOST_CHECK(!result);
 }
 
 BOOST_AUTO_TEST_CASE(bad_source_suffix)
 {
-	char* result = translate_file("existing_file.txt", "aaa", "txt");
+	result = translate_file("existing_file.txt", "aaa", "txt");
 	BOOST_CHECK(!result);
 }
 
 BOOST_AUTO_TEST_CASE(bad_target_suffix)
 {
-	char* result = translate_file("existing_file.txt", "xxx", "aaa");
+	result = translate_file("existing_file.txt", "xxx", "aaa");
 	BOOST_CHECK(!result);
 }
 
