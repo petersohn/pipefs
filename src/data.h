e67fa05855eef787f2dc1526de0e32119b0f08dc
Péter Szabados <peter.szabados@ericsson.com>
2015-02-12 16:05:41 +0100

    Add global variables for prefix and suffix

--- src/data.h
+++ src/data.h
@@ -4,7 +4,10 @@
 struct pipefs_data {
     FILE* logfile;
     char* rootdir;
+    char* source_suffix;
+    char* target_suffix;
 };
+
 
 #define GET_DATA ((struct pipefs_data *) fuse_get_context()->private_data)
 
