f6bbdbaaa2763aa6b07d108106a1a65055faebff
Peter Szabados <kangirigungi@gmail.com>
2015-02-22 14:22:10 +0100

    Correct test case

--- test/testcases/can_write_to_non_suffix_files_on_mountpoint.sh
+++ test/testcases/can_write_to_non_suffix_files_on_mountpoint.sh
@@ -4,7 +4,7 @@ function test_run()
 {
     local file_contents="these are the file contents\nsecond line\n"
     local filename=filename.0
-	touch rootdir=$filename
+	touch rootdir/$filename
     echo -n "$file_contents" >mountpoint/$filename
     local read_file_contents="$(cat rootdir/$filename)"
     assert [ "$file_contents" == "$read_file_contents" ]
