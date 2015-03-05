28136c4d7400ebc8692fe87c81b7bfd90b8e69ec
Péter Szabados <peter.szabados@ericsson.com>
2015-03-05 16:40:43 +0100

    Add C binding for Caches

--- src/cache/pipefs_cache.cpp
+++ src/cache/pipefs_cache.cpp
@@ -1,6 +1,7 @@
 #include "pipefs_cache.h"
 
 #include "Cache.hpp"
+#include "Caches.hpp"
 #include "ReadLoop.hpp"
 
 #include <memory>
@@ -25,6 +26,40 @@ int pipefs_cache_read(const struct pipefs_cache* cache, void* buf,
 		size_t length, size_t position)
 {
 	return reinterpret_cast<const pipefs::Cache*>(cache)->read(buf, length, position);
+}
+
+struct pipefs_caches {
+	int dummy;
+};
+
+struct pipefs_caches* pipefs_caches_create()
+{
+	return reinterpret_cast<pipefs_caches*>(new pipefs::Caches{});
+}
+
+void pipefs_caches_destroy(struct pipefs_caches* caches)
+{
+	delete reinterpret_cast<pipefs::Caches*>(caches);
+}
+
+int pipefs_caches_get(struct pipefs_caches* caches, const char* key,
+		struct pipefs_cache** cache)
+{
+	auto result = reinterpret_cast<pipefs::Caches*>(caches)->get(key);
+	if (cache) {
+		*cache = reinterpret_cast<pipefs_cache*>(&result.first);
+	}
+	return result.second;
+}
+
+void pipefs_caches_release(struct pipefs_caches* caches, const char* key)
+{
+	reinterpret_cast<pipefs::Caches*>(caches)->release(key);
+}
+
+void pipefs_caches_cleanup(pipefs_caches* caches, size_t target_size)
+{
+	reinterpret_cast<pipefs::Caches*>(caches)->cleanup(target_size);
 }
 
 struct pipefs_readloop {
--- src/cache/pipefs_cache.h
+++ src/cache/pipefs_cache.h
@@ -14,6 +14,15 @@ void pipefs_cache_destroy(struct pipefs_cache* cache);
 int pipefs_cache_read(const struct pipefs_cache* cache, void* buf,
 		size_t length, size_t position);
 
+struct pipefs_caches;
+
+struct pipefs_caches* pipefs_caches_create();
+void pipefs_caches_destroy(struct pipefs_caches* caches);
+int pipefs_caches_get(struct pipefs_caches* caches, const char* key,
+		struct pipefs_cache** cache);
+void pipefs_caches_release(struct pipefs_caches* caches, const char* key);
+void pipefs_caches_cleanup(struct pipefs_caches* caches, size_t target_size);
+
 struct pipefs_readloop;
 
 struct pipefs_readloop* pipefs_readloop_create();
