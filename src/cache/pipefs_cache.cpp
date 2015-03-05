52863cdfdfb27d7048e8c2e106ceb1eb455240ce
Péter Szabados <peter.szabados@ericsson.com>
2015-03-05 16:08:29 +0100

    Refactor C representation of C++ types

--- src/cache/pipefs_cache.cpp
+++ src/cache/pipefs_cache.cpp
@@ -8,59 +8,59 @@
 extern "C" {
 
 struct pipefs_cache {
-	pipefs::Cache cache;
+	int dummy;
 };
 
 struct pipefs_cache* pipefs_cache_create()
 {
-	return new pipefs_cache{};
+	return reinterpret_cast<pipefs_cache*>(new pipefs::Cache{});
 }
 
 void pipefs_cache_destroy(struct pipefs_cache* cache)
 {
-	delete cache;
+	delete reinterpret_cast<const pipefs::Cache*>(cache);
 }
 
 int pipefs_cache_read(const struct pipefs_cache* cache, void* buf,
 		size_t length, size_t position)
 {
-	return cache->cache.read(buf, length, position);
+	return reinterpret_cast<const pipefs::Cache*>(cache)->read(buf, length, position);
 }
 
 struct pipefs_readloop {
-	pipefs::ReadLoop readLoop;
+	int dummy;
 };
 
 struct pipefs_readloop* pipefs_readloop_create()
 {
-	return new pipefs_readloop{};
+	return reinterpret_cast<pipefs_readloop*>(new pipefs::ReadLoop{});
 }
 
 void pipefs_readloop_destroy(struct pipefs_readloop* readloop)
 {
-	delete readloop;
+	delete reinterpret_cast<pipefs::ReadLoop*>(readloop);
 }
 
 
 void pipefs_readloop_start(struct pipefs_readloop* readloop)
 {
-	readloop->readLoop.start();
+	reinterpret_cast<pipefs::ReadLoop*>(readloop)->start();
 }
 
 void pipefs_readloop_stop(struct pipefs_readloop* readloop)
 {
-	readloop->readLoop.stop();
+	reinterpret_cast<pipefs::ReadLoop*>(readloop)->stop();
 }
 
-void pipefs_readloop_add(struct pipefs_readloop* readloop, int fd, 
+void pipefs_readloop_add(struct pipefs_readloop* readloop, int fd,
 		struct pipefs_cache* cache)
 {
-	readloop->readLoop.add(fd, cache->cache);
+	reinterpret_cast<pipefs::ReadLoop*>(readloop)->add(fd, *reinterpret_cast<pipefs::Cache*>(cache));
 }
 
 void pipefs_readloop_remove(struct pipefs_readloop* readloop, int fd)
 {
-	readloop->readLoop.remove(fd);
+	reinterpret_cast<pipefs::ReadLoop*>(readloop)->remove(fd);
 }
 
 }
