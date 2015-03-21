13fd2497623d44b1592aa3c07ab419c6e9dbd45c
Peter Szabados <kangirigungi@gmail.com>
2015-03-21 17:34:12 +0100

    Make ReadLoop mockable

---
+++ src/cache/BasicReadLoop.hpp
@@ -0,0 +1,113 @@
+#ifndef SRC_CACHE_BASICREADLOOP_HPP
+#define SRC_CACHE_BASICREADLOOP_HPP
+
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/buffer.hpp>
+#include <map>
+#include <functional>
+
+#include "log.h"
+
+namespace pipefs {
+
+class Cache;
+
+template <typename StreamDescriptor>
+class BasicReadLoop {
+public:
+	using ReadStarter = std::function<int()>;
+
+	BasicReadLoop(boost::asio::io_service& ioService):
+		ioService(ioService) {}
+	void cancel()
+	{
+		for (auto& value: caches) {
+			value.second.stream.cancel();
+		}
+	}
+
+	void add(ReadStarter readStarter, Cache& cache)
+	{
+		int fd = readStarter();
+		log_msg("ReadLoop::add(%d)\n", fd);
+		auto emplaceResult = caches.emplace(fd,
+				CacheData{cache, {ioService, fd}, ""});
+
+		if (emplaceResult.second) {
+			auto& data = emplaceResult.first->second;
+			startReading(data);
+		}
+	}
+
+	void remove(int fd)
+	{
+		log_msg("ReadLoop::remove(%d)\n", fd);
+		auto it = caches.find(fd);
+		if (it != caches.end()) {
+			auto& data = it->second;
+			boost::system::error_code errorCode;
+			data.stream.cancel(errorCode);
+			// ignore the error
+
+			caches.erase(it);
+		}
+	}
+
+private:
+	constexpr static std::size_t bufferSize = 65536;
+
+	boost::asio::io_service& ioService;
+
+	struct CacheData {
+		Cache& cache;
+		StreamDescriptor stream;
+		char buffer[bufferSize];
+
+		CacheData(CacheData&&) = default;
+		CacheData& operator=(CacheData&&) = default;
+	};
+
+	std::map<int, CacheData> caches;
+
+	void startReading(CacheData& data)
+	{
+		//log_msg("ReadLoop::startReading(%d)\n", data.stream.native_handle());
+		using std::placeholders::_1;
+		using std::placeholders::_2;
+		data.stream.async_read_some(
+				boost::asio::buffer<char, bufferSize>(data.buffer),
+				std::bind(&BasicReadLoop::readFinished, this, std::ref(data), 
+					_1, _2));
+	}
+
+	void readFinished(CacheData& data, boost::system::error_code errorCode,
+			std::size_t bytesTransferred)
+	{
+		//log_msg("ReadLoop::readFinished(fd=%d, error=%s, bytes=%lu)\n",
+				//data.stream.native_handle(), errorCode.message().c_str(),
+				//bytesTransferred);
+
+		if (!errorCode) {
+			if (bytesTransferred > 0) {
+				data.cache.write(data.buffer, bytesTransferred);
+				startReading(data);
+				return;
+			}
+		} else {
+		}
+
+		data.cache.finish();
+		int key = data.stream.native_handle();
+		if (data.stream.is_open()) {
+			boost::system::error_code errorCode;
+			data.stream.close(errorCode);
+			// ignore the error
+		}
+
+		remove(key);
+	}
+};
+
+}
+
+#endif /* SRC_CACHE_BASICREADLOOP_HPP */
--- src/cache/ReadLoop.hpp
+++ src/cache/ReadLoop.hpp
@@ -1,44 +1,13 @@
 #ifndef SRC_CACHE_READLOOP_HPP
 #define SRC_CACHE_READLOOP_HPP
 
-#include <boost/asio/io_service.hpp>
 #include <boost/asio/posix/stream_descriptor.hpp>
-#include <map>
-#include <functional>
+
+#include "BasicReadLoop.hpp"
 
 namespace pipefs {
 
-class Cache;
-
-class ReadLoop {
-public:
-	using ReadStarter = std::function<int()>;
-
-	ReadLoop(boost::asio::io_service& ioService):
-		ioService(ioService) {}
-	void cancel();
-	void add(ReadStarter readStarter, Cache& cache);
-	void remove(int fd);
-private:
-	constexpr static std::size_t bufferSize = 65536;
-
-	boost::asio::io_service& ioService;
-
-	struct CacheData {
-		Cache& cache;
-		boost::asio::posix::stream_descriptor stream;
-		char buffer[bufferSize];
-
-		CacheData(CacheData&&) = default;
-		CacheData& operator=(CacheData&&) = default;
-	};
-
-	std::map<int, CacheData> caches;
-
-	void startReading(CacheData& data);
-	void readFinished(CacheData& data, boost::system::error_code errorCode,
-			std::size_t bytesTransferred);
-};
+using ReadLoop = BasicReadLoop<boost::asio::posix::stream_descriptor>;
 
 }
 
